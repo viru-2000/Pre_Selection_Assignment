9f7625698416e1d48968fbab859240335bd20b66
viru-2000 <virajrasal93@gmail.com>
2024-03-06 13:06:46 +0530

    Day2

---
+++ day2/PatternPrinter.java
@@ -0,0 +1,18 @@
+/* a)Print the following patterns using loop :
+a.
+*
+**
+***
+**** */
+
+public class PatternPrinter {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
